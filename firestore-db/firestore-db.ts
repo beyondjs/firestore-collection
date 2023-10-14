6f47da1cceba621356d1e80a3bb468bc9baaacd4
Henry Box <14365005+henrybox@users.noreply.github.com>
2023-10-14 23:17:08 +0000

    Initial setup

---
+++ firestore-db/firestore-db.ts
@@ -0,0 +1,14 @@
+import * as fs from 'fs';
+import { join } from 'path';
+import * as admin from 'firebase-admin';
+import { initializeApp } from 'firebase-admin/app';
+import type { Credential } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+
+const file = join(process.cwd(), './credentials/gcloud.json');
+
+const specs: { credential?: Credential } = {};
+specs.credential = fs.existsSync(file) ? admin.credential.cert(file) : void 0;
+initializeApp(specs.credential ? specs : void 0);
+
+export /*bundle*/ const db = getFirestore();
