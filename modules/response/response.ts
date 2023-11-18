080aea860654ff1e1b100be77b9e0912c764afd3
Henry Box <14365005+henrybox@users.noreply.github.com>
2023-11-18 20:43:42 +0000

    improvement: ERROR type in response

--- modules/response/response.ts
+++ modules/response/response.ts
@@ -1,13 +1,8 @@
 import { FirestoreErrorManager } from '@beyond-js/firestore-collection/errors';
 import { Response as ResponseBase } from '@beyond-js/response/main';
 
-export /*bundle*/ class Response<DataType> extends ResponseBase<DataType> {
-	#error: FirestoreErrorManager;
-	get error() {
-		return this.#error;
-	}
-
-	constructor(params: { error?: FirestoreErrorManager; data?: DataType }) {
+export /*bundle*/ class Response<DATA> extends ResponseBase<DATA, FirestoreErrorManager> {
+	constructor(params: { error?: FirestoreErrorManager; data?: DATA }) {
 		super(params);
 	}
 }
